01ab1ef8094b263641711485b1eef317cf758e4d
Philip <pappiahkubi1@gmail.com>
2021-06-16 13:16:05 +0000

    Updates

---
+++ precache-manifest.a1508eb98f92044f1fff5668eb1c08cd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eea7fa8e7897adbc8c7d707b74aae0b2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0166c3e24c925afe0475",
+    "url": "./static/js/2.c5d3adab.chunk.js"
+  },
+  {
+    "revision": "aacb2b4ac3dc20be49bd8c86fbd4ec8e",
+    "url": "./static/js/2.c5d3adab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4c883ddd0ccc31e868c",
+    "url": "./static/js/main.386321ea.chunk.js"
+  },
+  {
+    "revision": "df727c78dea0b716a2de",
+    "url": "./static/js/runtime-main.12e00ac1.js"
+  },
+  {
+    "revision": "73dabb371ddce7c640334fe0904dcddf",
+    "url": "./static/media/eth.73dabb37.png"
+  },
+  {
+    "revision": "f14b1d2f421e14b93efb183d0cbf768b",
+    "url": "./static/media/logo.f14b1d2f.svg"
+  },
+  {
+    "revision": "0e48903e734324d3fa4ae30ce51535dd",
+    "url": "./static/media/logo_white.0e48903e.svg"
+  },
+  {
+    "revision": "3a8f3f283ff60745678c5c5613bc3f35",
+    "url": "./static/media/wordmark_white.3a8f3f28.svg"
+  }
+]);
